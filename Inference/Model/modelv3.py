fe27492c032a519b7cc3f10cf766d55aa8d88c31
TomasConti02 <tomasconti34@gmail.com>
2026-09-02 10:17:52 +0200

    uint8 in input

--- Inference/Model/modelv3.py
+++ Inference/Model/modelv3.py
@@ -1,29 +1,33 @@
 import keras
 import tensorflow as tf
+from classification_models.tfkeras import Classifiers
 from keras import layers
 
-# Prepare the trained resnet model for production into KServe using Keras 3's ExportArchive API
 base_trained_model = keras.models.load_model("resnet18_68C_embedding_layer_v5_OK.keras")
 embedding_output = base_trained_model.get_layer("embedding_norm").output
 predicted_class_output = layers.Lambda(lambda x: tf.argmax(x, axis=1, output_type=tf.int32), name="predicted_class")(base_trained_model.output)
+
 multi_output_model = keras.Model(
     inputs=base_trained_model.inputs, 
     outputs={"predicted_class": predicted_class_output, "embedding": embedding_output}
 )
-multi_output_model.summary()
 
 export_archive = keras.export.ExportArchive()
-export_archive.track(multi_output_model)  # Needed for the weights
+export_archive.track(multi_output_model) 
+
+_, preprocess_input = Classifiers.get('resnet18')
 
 @tf.function
 def serve_fn(x):
-    return multi_output_model(x, training=False) #IMPORTANT
+    x_float = tf.cast(x, tf.float32) #cast to float32
+    x_preprocessed = preprocess_input(x_float) #norm as in training
+    return multi_output_model(x_preprocessed, training=False)
 
 export_archive.add_endpoint(
     name="serving_default",
     fn=serve_fn,
     input_signature=[
-        tf.TensorSpec(shape=(None, 256, 256, 3), dtype=tf.float32, name="input_image")
+        tf.TensorSpec(shape=(None, 256, 256, 3), dtype=tf.uint8, name="input_image")
     ]
 )
 
